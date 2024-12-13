f8b6e899f2bd46f03e6b20488ebf1b8b6b36e7f8
mintkat <lucasbear9@gmail.com>
2024-12-13 13:23:11 -0900

    Move api into haxelib libary

---
+++ api/lucidware/godot/GodotObject.hx
@@ -0,0 +1,10 @@
+package lucidware.godot;
+
+import lucidware.core.*;
+
+@:native("godot.Object")
+extern class GodotObjectNative extends NativeObject {}
+
+class GodotObject extends InstanceObject {
+    
+}
